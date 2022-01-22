ff16c2b875fa539709393ff942d75406b5b816a2
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-01-22 17:41:48 +0300

    Add index.js

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import getName, { getAnswer } from './cli.js';
+
+const say = console.log;
+
+export const greetUser = () => {
+  say('Welcome to the Brain Games!');
+  const userName = getName();
+  say(`Hello, ${userName}!`);
+
+  return userName;
+};
+
+const engine = (getArgs, getCorrectAnswer) => {
+  let gamesCount = 1;
+  let gameResult;
+
+  do {
+    const args = getArgs();
+    say(`Question: ${args.join(' ')}`);
+
+    const correctAnswer = getCorrectAnswer(args);
+    const userAnswer = getAnswer();
+    gameResult = `${userAnswer}` === `${correctAnswer}`;
+
+    if (gameResult) {
+      say('Correct!');
+      gamesCount += 1;
+    } else {
+      say(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
+    }
+  } while (gameResult && gamesCount <= 3);
+
+  return gameResult;
+};
+
+const sayByeToUser = (userName, gameResult) => {
+  if (gameResult) {
+    say(`Congratulations, ${userName}!`);
+  } else {
+    say(`Let's try again, ${userName}!`);
+  }
+};
+
+export default (task, getArgs, getCorrectAnswer) => {
+  const userName = greetUser();
+  say(`${task}`);
+  const gameResult = engine(getArgs, getCorrectAnswer);
+  sayByeToUser(userName, gameResult);
+};
