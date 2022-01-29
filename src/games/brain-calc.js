97c6b36543a08c9b9a4c85aa26668a5fbc4c3fbf
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-01-29 21:50:30 +0300

    Update brain-calc.js
    Add getCorrectAnswer function to separate calculator

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -3,10 +3,7 @@ import getRandomNumber from '../utils.js';
 
 const operators = ['+', '-', '*'];
 const getOperator = () => operators[getRandomNumber(0, 2)];
-
-const task = 'What is the result of the expression?';
-const getArgs = () => [getRandomNumber(), getOperator(), getRandomNumber()];
-const calcNumbers = ([num1, operator, num2]) => {
+const calcNumbers = (num1, operator, num2) => {
   switch (operator) {
     case '+':
       return num1 + num2;
@@ -17,4 +14,8 @@ const calcNumbers = ([num1, operator, num2]) => {
   }
 };
 
-export default () => gameLogic(task, getArgs, calcNumbers);
+const task = 'What is the result of the expression?';
+const getArgs = () => [getRandomNumber(), getOperator(), getRandomNumber()];
+const getCorrectAnswer = (args) => calcNumbers(...args);
+
+export default () => gameLogic(task, getArgs, getCorrectAnswer);
