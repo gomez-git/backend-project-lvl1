bfec55ffa3081189fb71daa1a483962f6e9d9cdb
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-01-29 21:49:08 +0300

    Update brain-prime.js
    Separate clean function isPrime from getCorrectAnswer

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,19 +1,18 @@
 import gameLogic from '../index.js';
 import getRandomNumber from '../utils.js';
 
-const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-const getArgs = () => [getRandomNumber()];
 const isPrime = (num) => {
-  if (num === 1) {
-    return 'no';
-  }
   for (let i = 2; i <= num / 2; i += 1) {
     if (num % i === 0) {
-      return 'no';
+      return false;
     }
   }
 
-  return 'yes';
+  return num > 1;
 };
 
-export default () => gameLogic(task, getArgs, isPrime);
+const task = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const getArgs = () => [getRandomNumber()];
+const getCorrectAnswer = ([num]) => (isPrime(num) ? 'yes' : 'no');
+
+export default () => gameLogic(task, getArgs, getCorrectAnswer);
