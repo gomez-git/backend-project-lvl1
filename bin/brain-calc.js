87acb40755ebdcaef249f71b450443e3d8fea3f9
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-05-17 20:05:29 +0300

    Delete old files

--- bin/brain-calc.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import startGame from '../src/games/brain-calc.js';
-
-startGame();
--- bin/brain-gcd.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import startGame from '../src/games/brain-gcd.js';
-
-startGame();
