3d04da8ca09d7960221719d8fbf249b519b93327
gomez-git <78104957+gomez-git@users.noreply.github.com>
2022-05-22 20:59:21 +0300

    Make map as static property of class

--- src/entities/games/BrainMorse.js
+++ src/entities/games/BrainMorse.js
@@ -4,54 +4,55 @@ export default class BrainMorse extends Engine {
   constructor(options) {
     super(options);
     this.task = 'Transform Morse char to English char or digit.';
-    this.map = {
-      '.': 'e',
-      _: 't',
-      '..': 'i',
-      '._': 'a',
-      '_.': 'n',
-      __: 'm',
-      '...': 's',
-      '._.': 'r',
-      '.._': 'u',
-      '_..': 'd',
-      '.__': 'w',
-      '_._': 'k',
-      '__.': 'g',
-      ___: 'o',
-      '....': 'h',
-      '..._': 'v',
-      '.._.': 'f',
-      '._..': 'l',
-      '_...': 'b',
-      '.__.': 'p',
-      '_.._': 'x',
-      '_._.': 'c',
-      '__..': 'z',
-      '.___': 'j',
-      '_.__': 'y',
-      '__._': 'q',
-      _____: 0,
-      '.____': 1,
-      '..___': 2,
-      '...__': 3,
-      '...._': 4,
-      '.....': 5,
-      '_....': 6,
-      '__...': 7,
-      '___..': 8,
-      '____.': 9,
-    };
   }
 
+  static map = {
+    '.': 'e',
+    _: 't',
+    '..': 'i',
+    '._': 'a',
+    '_.': 'n',
+    __: 'm',
+    '...': 's',
+    '._.': 'r',
+    '.._': 'u',
+    '_..': 'd',
+    '.__': 'w',
+    '_._': 'k',
+    '__.': 'g',
+    ___: 'o',
+    '....': 'h',
+    '..._': 'v',
+    '.._.': 'f',
+    '._..': 'l',
+    '_...': 'b',
+    '.__.': 'p',
+    '_.._': 'x',
+    '_._.': 'c',
+    '__..': 'z',
+    '.___': 'j',
+    '_.__': 'y',
+    '__._': 'q',
+    _____: 0,
+    '.____': 1,
+    '..___': 2,
+    '...__': 3,
+    '...._': 4,
+    '.....': 5,
+    '_....': 6,
+    '__...': 7,
+    '___..': 8,
+    '____.': 9,
+  };
+
   getRandomArgs() {
-    const chars = Object.keys(this.map);
+    const chars = Object.keys(this.constructor.map);
     const char = chars[this.getRandomNum(0, chars.length - 1)];
 
     return [char];
   }
 
   getCorrectAnswer([char]) {
-    return this.map[char];
+    return this.constructor.map[char];
   }
 }
